6e9fda9ccc050371355d33beb674788b495cbc5e
Matt Cowley <me@mattcowley.co.uk>
2023-01-11 16:38:05 +0000

    Fix microbundle output, generate UMD only (#18)

--- src/embed.js
+++ src/embed.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 DigitalOcean
+Copyright 2023 DigitalOcean
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,10 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import records from './records';
-import fetch from './fetch';
-import refreshIcon from './icons/refresh-cw';
-import externalLinkIcon from './icons/external-link';
+import records from './records.js';
+import fetch from './fetch.js';
+import refreshIcon from './icons/refresh-cw.js';
+import externalLinkIcon from './icons/external-link.js';
 
 export default class DNSEmbed {
     /**

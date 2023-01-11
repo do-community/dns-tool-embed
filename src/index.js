e6ebf9088f3a4161cff198958484796536248378
MattIPv4 <me@mattcowley.co.uk>
2023-01-11 15:48:56 +0000

    Output a single UMD bundle during build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 DigitalOcean
+Copyright 2023 DigitalOcean
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import DNSEmbed from './embed';
+import DNSEmbed from './embed.js';
 
 window._DNSToolEmbedsCache = {};
 
